df47f6c30725e65b2582004561d229e0a7f5692b
Onyiloves <onyinyemuoneme@gmail.com>
2023-06-20 09:43:55 -0700

    command that prints 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
-nclude "main.h"
-
+#include <stdio.h>
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
+ * main-Starts here
+ * description-prints _putchar
+ * Return:0
+ */
+int main(void)
 {
-	        return (write(1, &c, 1));
+	printf("_putchar\n");
+	return (0);
 }
