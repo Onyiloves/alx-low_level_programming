c74bfb7e6ce5a040810c14709a50ce8bb53c8f51
Onyiloves <onyinyemuoneme@gmail.com>
2023-10-16 14:32:37 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+int add(int a,int b)
+{
+	return a + b;
+}
+
+int sub(int a,int b)
+{
+	return a - b;
+}
+
+int mul(int a,int b)
+{
+	return a * b;
+}
+
+
+int div(int a,int b)
+{
+	return a / b;
+}
+
+int mod(int a,int b)
+{
+	return a % b;
+}
